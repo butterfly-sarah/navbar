3a0180921f4183e97368ed5cdc447ef6ccb6edec
butterfly-sarah <sarahmohsen761@gmail.com>
2022-12-26 17:56:46 +0200

    first commit

---
+++ nav.js
@@ -0,0 +1,17 @@
+div=document.querySelector("div");
+span=document.querySelectorAll("span");
+function showList(){
+    div.style.width="260px";
+    var i;
+    for (i = 0; i < span.length; i++) {
+        span[i].style.display="inline-block";
+        span[i].style.margin="auto";
+        span[i].style.width="200px";
+    }
+}
+function out(){
+    div.style.width="55px";
+    for (i = 0; i < span.length; i++) {
+        span[i].style.display="none";
+    }
+}
